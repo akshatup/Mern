cb0e4d965c0029b1bd2d1281bb0d2be6e0dd29a3
akshatup <akshatupasani13@gmailcom>
2025-12-11 04:38:48 +0530

    scoping

---
+++ JavaScript/scoping.js
@@ -0,0 +1,28 @@
+//global scope
+
+// let a = 10;
+
+// {
+//   var a = 10;
+// //   console.log(a);
+// }
+// console.log(a);
+
+// function print() {
+//   let a = 10;
+// //   console.log(a);
+// }
+// print();
+// console.log(a);
+
+function outer() {
+  let value = 10;
+
+  function inner() {
+    console.log(value);
+  }
+  return inner;
+}
+let res = outer();
+res();
+console.log(res);
