022e4b8ff338d6933ac1e8960db884b804f7fe09
akshatup <akshatupasani13@gmailcom>
2025-12-11 04:38:14 +0530

    IIFE

---
+++ JavaScript/IIFE.js
@@ -0,0 +1,9 @@
+// IIFE (Immediately Invoked Function Expression)
+
+// (function sayHi(name){
+//     console.log(`hi ${name}`);
+// })("akshat");
+
+(function sayHi(a, b){
+    console.log(a+b);
+})(2,7);
